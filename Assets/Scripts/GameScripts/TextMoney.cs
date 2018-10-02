82b744c67d830ee28b796e0b020bdae1fa3b36dc
ejik1332 <garabajii2002@gmail.com>
2018-10-02 23:01:04 +0300

    Initial commit

---
+++ Assets/Scripts/GameScripts/TextMoney.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMoney : MonoBehaviour {
+	static Text moneyText;
+	static long money;
+	// Use this for initialization
+	void Start () {
+		moneyText = GameObject.Find("Money").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		moneyText.text = "$ " + money;
+	}
+
+	public static void AdjustCurrentValue(long moneyl)
+	{
+		money += moneyl;
+	}
+
+	public static long currentMoney
+	{
+		get{ return money; }
+	}
+}
